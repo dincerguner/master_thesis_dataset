524a204a8a7d643917c02f35bdb3db8651a26300
Dincer Guner <erdincerguner55@gmail.com>
2022-02-27 19:36:55 +0200

    c

--- ant/jakarta-ant-1.3/bootstrap.bat
+++ ant/jakarta-ant-1.3/bootstrap.bat
@@ -21,7 +21,7 @@ echo ... Bootstrapping Ant Distribution
 if     "%OS%" == "Windows_NT" if exist bootstrap rmdir/s/q bootstrap
 if not "%OS%" == "Windows_NT" if exist bootstrap deltree/y bootstrap
 
-SET LOCALCLASSPATH=lib\parser.jar;lib\jaxp.jar;lib\optional\junit.jar
+SET LOCALCLASSPATH=lib\parser.jar;lib\jaxp.jar;lib\optional\junit.jar;lib\optional\regexp-1.3.jar;lib\optional\jakarta-oro-2.0.jar
 
 if exist %JAVA_HOME%\lib\tools.jar call src\script\lcp.bat %JAVA_HOME%\lib\tools.jar
 if exist %JAVA_HOME%\lib\classes.zip call src\script\lcp.bat %JAVA_HOME%\lib\classes.zip
@@ -48,36 +48,5 @@ echo ... Compiling Ant Classes
 
 %JAVAC% -d %CLASSDIR% %TOOLS%\tar\*.java %TOOLS%\ant\*.java %TOOLS%\ant\types\*.java %TOOLS%\ant\taskdefs\*.java %TOOLS%\ant\util\*.java %TOOLS%\ant\util\regexp\RegexpMatcher.java %TOOLS%\ant\util\regexp\RegexpMatcherFactory.java
 
-echo.
-echo ... Copying Required Files
-
-copy %TOOLS%\ant\taskdefs\*.properties %CLASSDIR%\org\apache\tools\ant\taskdefs
-copy %TOOLS%\ant\types\*.properties %CLASSDIR%\org\apache\tools\ant\types
-
-echo.
-echo ... Building Ant Distribution
-
-xcopy /s/q %CLASSDIR% build\classes
-
-%JAVA% %ANT_OPTS% org.apache.tools.ant.Main bootstrap
-
-echo.
-echo ... Cleaning Up Build Directories
-
-if     "%OS%" == "Windows_NT" if exist %CLASSDIR%\nul rmdir/s/q %CLASSDIR%
-if not "%OS%" == "Windows_NT" if exist %CLASSDIR%\nul deltree/y %CLASSDIR%
-
-echo.
-echo ... Done Bootstrapping Ant Distribution
-
-set JAVA=%OLDJAVA%
-set JAVAC=%OLDJAVAC%
-set CLASSPATH=%OLDCLASSPATH%
-set ANT_HOME=%OLDANTHOME%
-set OLDJAVA=
-set OLDJAVAC=
-set OLDCLASSPATH=
-set LOCALCLASSPATH=
-set OLDANTHOME=
-set TOOLS=
-
+%JAVAC% -d %CLASSDIR% %TOOLS%\ant\utils\regexp\JakartaRegexpMatcher.java
+%JAVAC% -d %CLASSDIR% %TOOLS%\ant\utils\regexp\JakartaOroMatcher.java
