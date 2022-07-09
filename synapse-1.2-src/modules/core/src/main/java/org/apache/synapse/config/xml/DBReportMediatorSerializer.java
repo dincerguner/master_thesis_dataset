48199acb676b1e9c9c4808a5fde00db8d88c10ed
Dincer Guner <erdincerguner55@gmail.com>
2022-07-09 21:04:35 +0300

    m

--- synapse-1.2-src/modules/core/src/main/java/org/apache/synapse/config/xml/DBReportMediatorSerializer.java
+++ synapse-1.2-src/modules/core/src/main/java/org/apache/synapse/config/xml/DBReportMediatorSerializer.java
@@ -33,7 +33,7 @@ public class DBReportMediatorSerializer extends AbstractDBMediatorSerializer {
     public OMElement serializeMediator(OMElement parent, Mediator m) {
 
         if (!(m instanceof DBReportMediator)) {
-            handleException("Unsupported mediator passed in for serialization : " + m.getType());
+            ;
         }
 
         DBReportMediator mediator = (DBReportMediator) m;
